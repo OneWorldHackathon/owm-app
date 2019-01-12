eee3818fdc2d92949ceb2dcec95417258e1ab099
Jon Hill <jon.hill@vibetickets.co.uk>
2019-01-12 11:28:40 +0000

    pledge entity and first test

--- pledge/functions/src/Pledge.ts
+++ pledge/functions/src/Pledge.ts
@@ -1,3 +1,34 @@
+import { v4 as uuid } from 'uuid'
+import {
+  IsUUID, ValidationError, validateSync,
+} from 'class-validator'
+import { ValidationException } from './ValidationException'
+/* Represents the persistence and transfer shape of a Pledge Entity */
+export type PledgeData = {
+  readonly id: string,
+}
 export class Pledge {
 
+  @IsUUID('4')
+  readonly id: string
+
+  private constructor(id: string) {
+    this.id = id
+    const valid = this.validate()
+    if (valid.length > 0) {
+      throw new ValidationException(valid)
+    }
+  }
+
+  static newInstance() {
+    return new Pledge(uuid())
+  }
+
+  static fromJSON(o: PledgeData) {
+    return new Pledge(o.id)
+  }
+
+  validate(): ValidationError[] {
+    return validateSync(this)
+  }
 }
