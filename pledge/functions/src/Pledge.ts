eb01d8846dda0667a0581679057dce02a5a65c72
Will Faithfull <will@sourcepulp.com>
2019-01-12 13:42:19 +0000

    Add pledge repository, make pledge fulfil FirestoreEntity contract

--- pledge/functions/src/Pledge.ts
+++ pledge/functions/src/Pledge.ts
@@ -4,21 +4,24 @@ import {
 } from 'class-validator'
 import { ValidationException } from './ValidationException'
 import { Conversions } from './Conversions'
+import { FirestoreEntity } from './Repository';
+import { toFirestore } from './utils';
+import { DocumentData } from '@google-cloud/firestore';
 /* Represents the persistence and transfer shape of a Pledge Entity */
 export type PledgeData = {
   readonly id: string,
   readonly distanceMetres: number,
 }
-export class Pledge {
+export class Pledge implements FirestoreEntity {
 
   @IsUUID('4')
-  readonly id: string
+  readonly _id: string
 
   @Min(500) @Max(42164.8128)
   private readonly _distanceMetres: number
 
-  private constructor(id: string, distanceMetres: number) {
-    this.id = id
+  private constructor(_id: string, distanceMetres: number) {
+    this._id = _id
     this._distanceMetres = distanceMetres
     const valid = this.validate()
     if (valid.length > 0) {
@@ -47,5 +50,20 @@ export class Pledge {
 
   get distanceMiles(): number {
     return Conversions.metresToMiles(this._distanceMetres)
+  }
+
+  toPledgeData(): PledgeData {
+    return {
+      id: this._id,
+      distanceMetres: this._distanceMetres,
+    }
+  }
+
+  toFirestore(): DocumentData {
+    return toFirestore(this.toPledgeData())
+  }
+
+  id(): string {
+    return this._id
   }
 }
---
+++ pledge/functions/src/PledgeRepository.ts
@@ -0,0 +1,9 @@
+import { Pledge } from './Pledge'
+import { Firestore } from '@google-cloud/firestore'
+import { AbstractFirestoreRepository } from './Repository';
+
+export class CloudFirestorePledgeRepository extends AbstractFirestoreRepository<Pledge> {
+  constructor(_db?: Firestore, _colPrefix: string = '') {
+    super('pledge', _db, _colPrefix)
+  }
+}
