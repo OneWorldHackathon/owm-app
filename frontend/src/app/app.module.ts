29943f94dd6f9010d9095dc5f5e3b1633f7d89dc
Jim Mann <jim.mann@vibetickets.co.uk>
2019-01-12 15:03:04 +0000

    Create a app-globe component.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GlobeComponent } from './globe/globe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlobeComponent
   ],
   imports: [
     BrowserModule
