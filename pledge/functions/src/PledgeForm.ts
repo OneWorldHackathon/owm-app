d06fa0185aed7ac599b8538a37671c62a9128850
Jon Hill <jon.hill@vibetickets.co.uk>
2019-01-12 17:11:17 +0000

    location object in User

--- pledge/functions/src/PledgeForm.ts
+++ pledge/functions/src/PledgeForm.ts
@@ -1,11 +1,14 @@
+export type Location = {
+  lat: number, lng: number, countryCode: string, description: string,
+}
 export type PledgeForm = {
 
   readonly userId: string,
   readonly firstName: string,
   readonly lastName: string
   readonly yearOfBirth: string,
-  readonly country: string,
   readonly place: string
   readonly pledge: number,
+  readonly location: Location,
 
 }
