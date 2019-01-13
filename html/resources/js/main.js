63e0a202eb0a9908f6f50b218d075a0e33bcd48e
Aidan Watt <aidan@hotfootdesign.co.uk>
2019-01-13 13:49:39 +0000

    Bit more

--- html/resources/js/main.js
+++ html/resources/js/main.js
@@ -25,4 +25,22 @@ $(function(){
             api.open()
         }
     })
+
+    $('.go-to-pledge').click(function (e){
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $('#pledge').offset().top
+        }, 2000);
+    });
+
+    $('.range').change(function() {
+        var meters = $(this).val();
+
+        var miles = meters / 1609.344;
+
+        $('.range-number').val(meters);
+
+        $('.miles-number').text(miles.toFixed(1));
+    });
 })
