edbec68f09669303e01a4dba41c8775ea732cf19
Jack Woodmansey <jack.woodmansey@vibetickets.co.uk>
2019-01-13 17:53:47 +0000

    prod build script

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDD7Lul_yWYzF1jYBSuTJUgMQn4U09UrAI',
+    authDomain: 'oneworldmarathon.firebaseapp.com',
+    databaseURL: 'https://oneworldmarathon.firebaseio.com',
+    projectId: 'oneworldmarathon',
+    storageBucket: 'oneworldmarathon.appspot.com',
+    messagingSenderId: '820477595207',
+  },
+}
